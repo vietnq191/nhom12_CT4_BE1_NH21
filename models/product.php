6f91ddd759db34c6a41fb557369fb814072e960a
VietNQV <>
2021-11-04 00:09:27 +0700

    first commit

---
+++ models/product.php
@@ -0,0 +1,38 @@
+<?php
+class Product extends Db
+{
+    public function getAllProducts()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products");
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    public function getProductById($id)
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products WHERE id = ?");
+        $sql->bind_param("i", $id);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    //Lấy 10 sản phẩm mới nhất
+    public function getNewProducts(){
+        $sql = self::$connection->prepare("SELECT * FROM products ORDER BY id DESC LIMIT 0,10");
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    public function search($keyword){
+        $sql = self::$connection->prepare("SELECT * FROM products WHERE `name` LIKE ?");
+        $keyword = "%$keyword%";
+        $sql->bind_param("s", $keyword);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+}
