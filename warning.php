209cdea2504f99845aebc40d41533f305a8261d7
Trần Phan Quang Minh <tranphanquangming1997@gmail.com>
2021-11-16 13:33:31 +0700

    Update warning.php

--- warning.php
+++ warning.php
@@ -9,7 +9,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/login.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
 
     <title>Store</title>
@@ -34,6 +34,6 @@ session_start();
 
 </body>
 <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-<script src="js/app.js"></script>
+<script src="js/login.js"></script>
 
 </html>
